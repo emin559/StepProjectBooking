01a890e4096b6456f429dfc88a768606ba25d7d2
Emin <israfilzadehemin@gmail.com>
2020-03-12 10:47:50 +0400

    flightGenReady

--- src/main/java/app/util/FlightGenerator.java
+++ src/main/java/app/util/FlightGenerator.java
@@ -4,20 +4,48 @@ import app.enums.Airport;
 
 import javax.print.attribute.standard.Destination;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.Random;
 
 public class FlightGenerator {
 
 
-  public static String generateFlight() {
+  public static String generateFlight(int count) {
     StringBuilder flight = new StringBuilder();
-
-    ArrayList<Airport> randomDestinations= new ArrayList<>();
+    Random rndm = new Random();
+    ArrayList<Airport> randomDestinations = new ArrayList<>();
     EnumSet.allOf(Airport.class).forEach(air -> randomDestinations.add(air));
+
+
+    int seats = 5;
+
+    for (int i = 0; i < count; i++) {
+      int randomDestIndex = rndm.nextInt(randomDestinations.size() - 1);
+      int randomDate = rndm.nextInt(15);
+      Airport randomDest = randomDestinations.get(randomDestIndex);
+
+      DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+      LocalDateTime localDateTime = LocalDateTime.now();
+      LocalDateTime futureLocalDateTime = localDateTime
+              .plusDays(randomDate)
+              .plusHours(randomDate)
+              .plusMinutes(randomDate);
+
+      String formattedLocalDateTime = futureLocalDateTime.format(dateTimeFormatter);
+
+      flight
+              .append("FROM: KYIV TO: ")
+              .append(randomDest).append(" | ")
+              .append(formattedLocalDateTime).append(" | ")
+              .append(seats).append("\n");
+    }
+    return flight.toString();
   }
 
   public static void main(String[] args) {
-    System.out.println(generateFlight());
+    System.out.println(generateFlight(15));
   }
+
 }
